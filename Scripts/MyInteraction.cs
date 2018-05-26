4cfdc0efcaf4fda8a48d4cf76ddd5f048a84f046
sayhitosandy <sanidhya15085@iiitd.ac.in>
2018-05-26 18:25:11 +0530

    Added files via Upload

---
+++ Scripts/MyInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyInteraction : MonoBehaviour {
+
+    public string SceneName;
+    public Color enterColor;
+    public Color exitColor;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<TextMesh>().color = exitColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PointerEnter() 
+    {
+        GetComponent<TextMesh>().color = enterColor;
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void PointerExit()
+    {
+        GetComponent<TextMesh>().color = exitColor;
+    }
+}
---
+++ Scripts/PlaneMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaneMovement : MonoBehaviour {
+    Camera cockpit;
+    float speed = 2f;
+    public float maxSpeed = 12f;
+    bool flag = false;
+    // Use this for initialization
+	void Start () {
+        cockpit = GetComponentInChildren<Camera>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            flag = true;
+        }
+        if (flag == true) 
+        {
+            transform.position += cockpit.transform.forward * speed * Time.deltaTime;
+            if (speed < maxSpeed)
+            {
+                speed += 0.5f;
+            }
+
+            if (cockpit.transform.eulerAngles.z >= 45f && cockpit.transform.eulerAngles.z <= 315f)
+            {
+                SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+            }
+
+        }
+	}
+}
